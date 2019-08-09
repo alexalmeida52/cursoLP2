dc778fff9e3c37e8cfabe6c74a449dac9049577e
José Alex Alves Pereira <alx52@A309-19L.imd.local>
2019-08-08 21:36:39 -0300

    Documentation with Javadoc

--- src/br/ufrn/imd/lpII/aula05/Main.java
+++ src/br/ufrn/imd/lpII/aula05/Main.java
@@ -2,6 +2,7 @@ package br.ufrn.imd.lpII.aula05;
 
 public class Main {
     public static void main(String[] args) {
-
+        Operacoes operacoes = new Operacoes();
+        System.out.println(operacoes.soma(3, 5));
     }
 }
---
+++ src/br/ufrn/imd/lpII/aula05/Operacoes.java
@@ -0,0 +1,21 @@
+package br.ufrn.imd.lpII.aula05;
+
+/**
+ * Classe Operações, responsável pelas operações matemáticas de apoio ao sistema
+ *
+ * @author Alex Alves
+ * @version 0.1
+ * @since 2019.2
+ *
+ */
+public class Operacoes {
+    /**
+     *
+     * @param a It's an integer to add with b
+     * @param b It's an integer to add with a
+     * @return Returns an integer corresponding to the sum
+     */
+    public int soma(int a, int b){
+        return a + b;
+    }
+}
